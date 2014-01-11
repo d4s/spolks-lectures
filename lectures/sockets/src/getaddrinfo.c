31862d402487f20c4b3b6b5653787c5bcf29744f
Denis Pynkin <dans@altlinux.org>
2014-01-11 14:20:08 +0300

    Fixed memory leak for getaddrinfo example

--- lectures/sockets/src/getaddrinfo.c
+++ lectures/sockets/src/getaddrinfo.c
@@ -4,21 +4,24 @@
 
 int main (void) {
     struct addrinfo hints = {AI_CANONNAME, AF_INET, SOCK_STREAM, IPPROTO_TCP,0, NULL, NULL, NULL};
-    struct addrinfo *addr_res;
+    struct addrinfo *addr_res, *addr;
     int res=0;
 
 	if (res=getaddrinfo ("google.by", "http", &hints, &addr_res) != 0) {
 	    perror ("GETADDRINFO");
 	    printf ("GETADDRINFO: %s\n", gai_strerror(res));
 	} else {
+	    addr = addr_res;
     	    printf ("Результаты для сервиса http\n"); 
 	    /* Пройдем по списку возвращенных структур */
     	    do {
-	        printf ("Адрес сокета: Порт: %d IP-адрес: %s\n",
-	    	    ntohs (((struct sockaddr_in *) addr_res->ai_addr)->sin_port),
-		    inet_ntoa (((struct sockaddr_in *) addr_res->ai_addr)->sin_addr));
-		printf ("Официальное имя хоста: %s\n", addr_res->ai_canonname);
-	    } while ((addr_res = addr_res->ai_next) != NULL);
+				printf ("Адрес сокета: Порт: %d IP-адрес: %s\n",
+					ntohs (((struct sockaddr_in *) addr->ai_addr)->sin_port),
+				    inet_ntoa (((struct sockaddr_in *) addr->ai_addr)->sin_addr));
+				printf ("Официальное имя хоста: %s\n", addr->ai_canonname);
+		    } while ((addr = addr->ai_next) != NULL);
+
+			freeaddrinfo( addr_res);
 	}
 
     return 0;
